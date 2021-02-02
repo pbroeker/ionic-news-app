081e39ab355588aef8475c327ef35eaca73e6629
pbroeker <broeker1986@gmail.com>
2021-02-02 11:22:45 +0100

    add: NewsService

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@angular/core';
 import { NewsElement } from '../Interfaces';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
+  private newsUrl = 'https://news-app-2e997.web.app/news';
+  private mockUrl = '../../assets/news.json';
+  constructor(
+    private http: HttpClient,
+  ) { }
 
-  constructor() { }
+  getNews(): Observable<any> {
+    return this.http.get<any>(this.newsUrl);
+  }
+
+  getMockNews(): Observable<any> {
+    return this.http.get<any>(this.mockUrl).pipe(map(
+      (response) => response.json()
+    ))
+  }
 }
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,11 +1,12 @@
 import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+
 
 @NgModule({
   imports: [
@@ -17,4 +18,6 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule{
+
+}
